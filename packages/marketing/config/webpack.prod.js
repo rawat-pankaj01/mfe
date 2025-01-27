737864d7580df3d7de086baeeb3409584fe2bf09
Pankaj Rawat <pankaj.rawat01@nagarro.com>
2025-01-27 12:28:43 +0530

    Added marketing deployement

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -5,7 +5,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
